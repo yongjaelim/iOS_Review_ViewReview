367e89504beda9a963036e27377c45bb35fa0d5f
Yongjae Lim <yongjae@vt.edu>
2022-10-05 21:37:32 -0700

    Initial Commit

---
+++ ViewReview/ViewReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewReviewApp.swift
+//  ViewReview
+//
+//  Created by Yongjae Lim on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ViewReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
